397786a5f39b6c8182ffca59e43c9dd688b897c6
jpcljpcljpcl <joaopaulolopes_@outlook.com>
2023-12-04 14:13:59 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jpcljpcljpcl.github.io',
-  base: '/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
