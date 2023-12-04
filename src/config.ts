51d64785f77e6fd55ddf7110bdd376d38952d4b8
jpcljpcljpcl <joaopaulolopes_@outlook.com>
2023-12-04 14:27:51 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'JoãoLopes.lnk';
+export const SITE_TITLE = 'JoãoLopes';
 export const SITE_DESCRIPTION = 'João Lopes Developer & Creator Personal Webspace';
